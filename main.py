95e945c5b18c2d97dbce6d0d1e573c02e53d1ecc
mylenaizidoro <izidoromylena@gmail.com>
2025-07-22 06:48:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,20 @@ import requests
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"status": "API funcionando!"}
-
-@app.get("/temperatura")
 def get_temperatura():
+    # URL do seu Firebase com o valor da temperatura
     firebase_url = "https://projetotemperaturaesp32-52b7d-default-rtdb.firebaseio.com/temperatura_ao_vivo.json"
+    
     try:
         response = requests.get(firebase_url)
-        response.raise_for_status()
+        response.raise_for_status()  # Lança erro se status != 200
+
         temperatura = response.json()
         if temperatura is None:
             return {"erro": "Temperatura não disponível"}
+
         temperatura = round(float(temperatura), 1)
         return {"temperatura": temperatura}
+
     except Exception as e:
         return {"erro": f"Falha ao acessar Firebase: {str(e)}"}
